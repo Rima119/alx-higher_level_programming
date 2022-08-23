0ea0bedbe48fe87831b4cc590a8541dbd9f92b9b
Rim El Filali <rimo1902@gmailcom>
2022-08-23 16:18:21 +0000

    solution to task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+if number >= 0:
+    lnum = number % 10
+else:
+    lnum = ((number * -1) % 10) * -1
+print('{}'.format(lnum), end='')
+return lnum
