6a559e0370d0b2cbd1715efddd7d925270c4bdd6
Rim El Filali <rimo1902@gmailcom>
2022-08-23 14:07:33 +0000

    solution to task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for n in range(10):
     for m in range(n + 1, 10):
-        print("{:d}{:d}".format(n, m), end=", ")
         if n == 8 and m == 9:
             print("{:d}{:d}".format(n, m))
+        else:
+            print("{:d}{:d}".format(n, m), end=", ") 
