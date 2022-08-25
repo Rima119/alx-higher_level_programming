2226735b936ec60dfd29ebc46aa940e4276edac3
RIM EL FILALI <rimo1902@gmail.com>
2022-08-25 06:01:44 -0700

    solution to task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    lnum = len(argv)
+    if lnum <= 1:
+        print("{:d} arguments.".format(lnum - 1))
+    elif lnum == 2:
+        print("{:d} argument:".format(lnum - 1))
+    else:
+        print("{:d} arguments:".format(lnum - 1))
+        for n in range(1, lnum):
+            print("{:d}: {}".format(n, argv[n]))
