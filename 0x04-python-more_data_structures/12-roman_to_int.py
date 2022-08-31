3d5b55edc5f7a4d460281e710a69e3d86babcd93
RIM EL FILALI <rimo1902@gmail.com>
2022-08-31 09:43:20 -0700

    solution to task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if type(roman_string) == str or roman_string is not None:
-        num = 0
+    num = 0
+    if type(roman_string) is str or roman_string is not None:
         for n in range(len(roman_string)):
             if (rom[roman_string[n]] > rom[roman_string[n - 1]] and n > 0):
                 num += rom[roman_string[n]] - 2 * rom[roman_string[n - 1]]
