e037e7121db01d2e987ca613761fae0a2192e606
RIM EL FILALI <rimo1902@gmail.com>
2022-09-12 09:26:15 -0700

    solution to task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     p = 0
-    try:
-        for n in range(x):
+    for n in range(x):
+        try:
             print("{}".format(my_list[n]), end="")
-    except:
-        break
-    else:
-        p += 1
+        except:
+            break
+        else:
+            p += 1
     print()
     return p
