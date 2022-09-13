90a4ecf8c090caf529fcaa6b4b37d016c4db52a0
RIM EL FILALI <rimo1902@gmail.com>
2022-09-13 15:07:18 -0700

    solution to task 10

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import math
+
+"""MagicClass Class"""
+
+
+
+class MagicClass:
+    """class that does exactly the same as the following Python bytecode"""
+    def __init__(self, radius=0):
+        self.__radius = 0
+        if type(radius) != int and type(radius) != float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        return ((self.__radius ** self.__redius) * math.pi)
+
+    def circumference(self):
+        return (2 * math.pi * self.__radius)
