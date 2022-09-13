ac145827ecdbccc7b03dbf3d8dbb9c3f7d62979f
RIM EL FILALI <rimo1902@gmail.com>
2022-09-13 03:59:04 -0700

    solution to task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    '''class Square that defines a square'''
+    def __init__(self, size):
+        '''
+            Instantiation with size
+        Args:
+            size: size of a square
+        '''
+        self.__size = size
