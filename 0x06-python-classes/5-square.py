4c3b3ad476f3a40cdf206b80c137e009f1049708
RIM EL FILALI <rimo1902@gmail.com>
2022-09-13 04:30:16 -0700

    solution to task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        """
+            Instantiation with size
+        Args:
+            size: size of a square
+        """
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """Returns size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Set size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def my_print(self):
+        """Prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print()
+        for n in range(self.__size):
+            for m in range(self.__size):
+                print("#", end="")
+            print()
