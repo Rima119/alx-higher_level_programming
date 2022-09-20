cf72db8bdde27c3035a28817bffad298d04b5547
RIM EL FILALI <rimo1902@gmail.com>
2022-09-20 08:34:58 -0700

    solution to task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""text_indentation module"""
+
+
+def text_indentation(text):
+    """function that prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+        text: string
+    """
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    n = text[:]
+    for m in ["?", ":", "."]:
+        txt = n.split(m)
+        n = ""
+        for s in txt:
+            s = s.strip(" ")
+            if n == "":
+                n = s + m
+            else:
+                n + "\n\n" + s + m
+    print(n[:-3], end="")
