3af33f924f2382754b69fad8c84bd0d62eca7edb
RIM EL FILALI <rimo1902@gmail.com>
2022-09-20 08:00:14 -0700

    solution to task 2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""say_my_name module"""
+
+
+
+def say_my_name(first_name, last_name=""):
+    """function that prints My name is <first name> <last name>
+
+    Args:
+        first_name: the first name (string)
+        last_name: the last name (string)
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name, last_name)
