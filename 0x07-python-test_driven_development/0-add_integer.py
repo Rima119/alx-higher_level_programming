3131940805b2e834aeb2d478571744d1fcd1e0a8
RIM EL FILALI <rimo1902@gmail.com>
2022-09-20 02:42:50 -0700

    solution to task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3a
+"""add_integer module"""
+
+
+def add_integer(a, b=98):
+    """function that adds 2 integers
+    
+    Args:
+        a: first number
+        b: second number
+    
+    Return:
+          The sum of the two numbers
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return (a + b)
