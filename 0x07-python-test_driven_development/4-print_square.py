a567d68a0dd525da4f8bca0ea7a5c2ea300c9b2f
RIM EL FILALI <rimo1902@gmail.com>
2022-09-20 08:06:40 -0700

    solution to task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""print_square module"""
+
+
+def print_square(size):
+    """ function that prints a square with the character #
+    
+    Args:
+        size: the size length of the square
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
+    for n in range(size):
+        print("#" * size)
