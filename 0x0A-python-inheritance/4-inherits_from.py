d9a81002a189fe24ebaa8cd0316ed078cb5fec3f
RIM EL FILALI <rimo1902@gmail.com>
2022-09-26 14:36:00 -0700

    solution to task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""inherits_from"""
+
+
+def inherits_from(obj, a_class):
+    """function that returns True if the object is an instance of
+    a class that inherited (directly or indirectly) from the specified class;
+    otherwise False
+    Args:
+        obj: an object
+        a_class: a class
+    """
+    return type(obj) != a_class and isinstance(obj, a_class)
