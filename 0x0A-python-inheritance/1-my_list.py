6ac8575a9995f4cd2408bf32973e5c37b36cb8a3
RIM EL FILALI <rimo1902@gmail.com>
2022-09-26 13:43:12 -0700

    solution to task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class MyList"""
+
+
+class MyList(list):
+    """Mylist Class"""
+    def print_sorted(self):
+        """prints the list, but sorted"""
+        print(sorted(self))
