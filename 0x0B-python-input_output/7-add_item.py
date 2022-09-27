ed54b84ffed0895b18a7cf79ba378bf2a99ee3d2
RIM EL FILALI <rimo1902@gmail.com>
2022-09-27 06:53:14 -0700

    solution to task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,11 +7,11 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
-    file = "add_item.json"
+    filename = "add_item.json"
     try:
-        list_p = load_from_json_file(file)
+        list_p = load_from_json_file(filename)
     except Exception:
         list_p = []
     for n in sys.argv[1:]:
         list_p.append(n)
-    save_to_json_file(list_p, file)
+    save_to_json_file(list_p, filename)
