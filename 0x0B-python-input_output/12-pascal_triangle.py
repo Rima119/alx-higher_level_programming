a9ab301f411bcac354732bccb61351549ebe043b
RIM EL FILALI <rimo1902@gmail.com>
2022-09-27 12:41:16 -0700

    solution to task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing
+    the Pascal’s triangle of n
+    """
+    rw = [1], tp = [0], m = []
+
+    if n <= 0:
+        return m
+    for k in range(n):
+        a.append(rw)
+        rw = [a+b for a, b in zip(rw + tp, tp + rw)]
+    return m
