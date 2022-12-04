e21f7f5ad365dd8fa2a3d46cf489f32cfad50fbb
Rim El Filali <rimo1902@gmail.com>
2022-12-04 13:11:30 -0800

    fix code

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -5,8 +5,8 @@ the database hbtn_0e_100_usa"""
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 
 
 if __name__ == "__main__":
