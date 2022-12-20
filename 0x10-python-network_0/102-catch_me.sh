d9c1316324b5c3fc013088358f483c482d823a30
Rim El Filali <rimo1902@gmail.com>
2022-12-20 13:00:15 -0800

    fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond
-curl -sL -H "X-School-User-Id: 98" -X PUT 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "user_id=98" -H "Origin:School" 0.0.0.0:5000/catch_me
