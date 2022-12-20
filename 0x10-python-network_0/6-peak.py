6bd45e4cc870dde00d336dfc015b6b8caea0879a
Rim El Filali <rimo1902@gmail.com>
2022-12-20 12:55:18 -0800

    add

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""find_peak module"""
+
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers."""
+    if (not list_of_integers) or len(list_of_integers) == 0:
+        return None
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        peak = list_of_integers[0]
+        return peak
